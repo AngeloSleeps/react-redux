969b11039b78eaca78941cb5b447660c7f5f42f5
Marcelo Magday <marcelo.angelo.magday@oocl.com>
2022-08-25 17:28:45 +0800

    Styles for UI now working.

--- src/features/Counter.js
+++ src/features/Counter.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import styles from './counter-style.css';
+import styles from './counter.module.css';
 
 import {
     increment,
